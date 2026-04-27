6770bcca549e50d4c2152a6df5bb82fcd467c173
1014739596 <juan.rojasl@uniagustiniana.edu.co>
2026-04-27 14:55:41 +0000

    Correcioness

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { BrowserRouter } from "react-router-dom";
+
+<BrowserRouter basename="/fakerapi">
+  {/* tus rutas */}
+</BrowserRouter>
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
